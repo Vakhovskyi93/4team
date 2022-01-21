1f6243a76495f1a2269b1139340d4059efacde63
oleksandrVakhovskyi <o.vakhovskyi@do-it.co>
2022-01-21 11:24:32 +0200

    add payment system icon

--- src/app/modules/order-view/page/order.component.ts
+++ src/app/modules/order-view/page/order.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CreditCardHendlerService } from 'src/app/services/credit-card-hendler.service';
 
 import { OrderService } from 'src/app/services/order.service';
 import { SpinerService } from 'src/app/services/spiner.service';
@@ -13,27 +14,45 @@ import { Card, Order } from 'src/app/utils/interfaces';
 export class OrderComponent implements OnInit {
   userOrder!: Order;
   product!: Card;
+  cardType: string = 'far fa-credit-card';
   constructor(
     private order: OrderService,
     private router: Router,
-    private spiner: SpinerService
+    private spiner: SpinerService,
+    private creditCardServise: CreditCardHendlerService
   ) {
+    /**
+     * if no order => navigate to homepage
+     * else get it
+     */
     this.order
       .getOrder()
       .pipe()
       .subscribe((data) =>
         data ? (this.userOrder = data) : this.router.navigateByUrl('')
       );
+
+    /**
+     * get product
+     *  */
     this.order.getProdact().subscribe((data) => {
       this.product = data;
     });
   }
 
   ngOnInit(): void {}
+
+  /**
+   * open spiner and navigate to next page after 20s
+   */
   submit() {
     this.spiner.show();
     setTimeout(() => {
       this.router.navigateByUrl('complete');
     }, 20000);
+  }
+
+  getCardIcon(){
+    return this.creditCardServise.setPaymentCompany(this.userOrder.cardNumber.slice(0,2))
   }
 }
